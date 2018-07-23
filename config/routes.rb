d593496c29b7acad39f532e502e7f4b9e03593e0
富田地広 <bwlmaniatomy@gmail.com>
2018-07-23 11:55:24 +0900

    add resources :comments(nest)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets
-  resources :users, only: :show
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
